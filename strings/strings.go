70cb811a3fe91f97591fcf8866730f6b6d12c0cb
Frank Kelly <kellyfj@gmail.com>
2026-02-11 15:31:38 -0500

    More stuff - strings and arrays

--- strings/strings.go
+++ strings/strings.go
@@ -1,7 +1,15 @@
 package strings
 
+import "strings"
+
 func reverse(in string) string {
 	return reverseRunes([]rune(in))
+}
+
+func reverseInPlace(r []rune) {
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
 }
 
 func reverseRunes(r []rune) string {
@@ -10,3 +18,79 @@ func reverseRunes(r []rune) string {
 	}
 	return string(r)
 }
+
+func isPalindrome(in string) bool {
+	r := []rune(in)
+	for i := 0; i < len(r)/2; i++ {
+		if r[i] != r[len(r)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func countVowels(in string) int {
+	vowels := map[rune]bool{
+		'a': true, 'e': true, 'i': true, 'o': true, 'u': true,
+		'A': true, 'E': true, 'I': true, 'O': true, 'U': true,
+	}
+	count := 0
+	for _, char := range in {
+		if vowels[char] {
+			count++
+		}
+	}
+	return count
+}
+
+func reverseAllWords(in string) string {
+	// NOTE: in the case where there is a trailing space `words`
+	// will have a space at the last element! Weird!
+	words := strings.Split(in, " ")
+	newWordsArray := make([]string, len(words))
+
+	for i, word := range words {
+		newWordsArray[(len(words)-1)-i] = word
+	}
+	return strings.Join(newWordsArray, " ")
+}
+
+// Reverse without using Split
+func reverseWordsWithoutSplit(in string) string {
+	r := []rune(in)
+	start := 0
+	for i := 0; i <= len(r); i++ {
+		if i == len(r) || r[i] == ' ' {
+			// reverse word from start to i-1
+			for j, k := start, i-1; j < k; j, k = j+1, k-1 {
+				r[j], r[k] = r[k], r[j]
+			}
+			start = i + 1
+		}
+	}
+	// reverse the entire string
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+// Reverse all words Order(1) space
+
+// func isMatch(regex string, s string)
+
+//  Compute Levenshtein Distance
+
+// Get all permutations of a string
+
+// Get the powerset of characters
+
+// Remove duplicate characters
+
+// Find longest palindrome
+
+//Longest common substring
+
+//Add number strings (with converting type)
+
+//Compute all mnemonics for a phone number
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -13,3 +13,52 @@ func TestReverse(t *testing.T) {
 	assert.Equal(t, reverse("a"), "a")
 	assert.Equal(t, reverse(""), "")
 }
+
+func TestIsPalindrome(t *testing.T) {
+	assert.True(t, isPalindrome("racecar"))
+	assert.True(t, isPalindrome("madam"))
+	assert.False(t, isPalindrome("hello"))
+	assert.True(t, isPalindrome("a"))
+	assert.True(t, isPalindrome(""))
+}
+
+func TestCountVowels(t *testing.T) {
+	assert.Equal(t, countVowels("hello"), 2)
+	assert.Equal(t, countVowels("Go is awesome!"), 6)
+	assert.Equal(t, countVowels("rhythm"), 0)
+	assert.Equal(t, countVowels("AEIOU"), 5)
+	assert.Equal(t, countVowels("aaeeiioouuAAEEIIOOUU"), 20)
+	assert.Equal(t, countVowels(""), 0)
+}
+
+func TestReverseInPlace(t *testing.T) {
+	r := []rune("hello")
+	reverseInPlace(r)
+	assert.Equal(t, string(r), "olleh")
+
+	r = []rune("Go is awesome!")
+	reverseInPlace(r)
+	assert.Equal(t, string(r), "!emosewa si oG")
+
+	r = []rune("racecar")
+	reverseInPlace(r)
+	assert.Equal(t, string(r), "racecar")
+
+	r = []rune("a")
+	reverseInPlace(r)
+	assert.Equal(t, string(r), "a")
+
+	r = []rune("")
+	reverseInPlace(r)
+	assert.Equal(t, string(r), "")
+}
+
+func TestReverseWords(t *testing.T) {
+	rev := reverseAllWords("able was I ere I saw elba")
+	assert.Equal(t, "elba saw I ere I was able", rev)
+
+	assert.Equal(t, "", reverseAllWords(""))
+	assert.Equal(t, "one", reverseAllWords("one"))
+	assert.Equal(t, "two one", reverseAllWords("one two"))
+	assert.Equal(t, " space extra one", reverseAllWords("one extra space "))
+}
